fdeb846fb57ce612ce0d3692d18da27f1cf10a7d
Erick Takeshi <erick.tmr@outlook.com>
2019-07-14 20:40:40 -0300

    implement not found response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::API
     'ActiveRecord::Relation',
     'ActiveRecord::Associations::CollectionProxy'
   ].freeze
+  CONTENT_TYPE = 'application/vnd.api+json'.freeze
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   protected
 
@@ -17,12 +20,20 @@ class ApplicationController < ActionController::API
       { is_collection: collection?(resources) }.merge(options)
     ).serializable_hash
 
-    render json: serialized_response, status: status, content_type: 'application/vnd.api+json'
+    render json: serialized_response, status: status, content_type: CONTENT_TYPE
   end
 
   private
 
   def collection?(resources)
     resources.is_a?(Array) || ACTIVE_RECORDS_COLLECTION_CLASSES.include?(resources.class.name)
+  end
+
+  def not_found_response
+    render json: {
+      errors: [
+        { source: :resource, title: 'Requested resource not found.' }
+      ]
+    }, status: :not_found, content_type: CONTENT_TYPE
   end
 end
