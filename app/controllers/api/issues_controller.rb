d4abb7a4964348bf403e775220439691760444be
Erick Takeshi <erick.tmr@outlook.com>
2019-07-14 20:17:11 -0300

    issues index

--- app/controllers/api/issues_controller.rb
+++ app/controllers/api/issues_controller.rb
@@ -1,11 +1,16 @@
 module Api
   class IssuesController < ApplicationController
     def index
-      
+      render_json_response(
+        resources: issues,
+        serializer: IssueSerializer
+      )
     end
 
-    def show
-      
+    private
+
+    def issues
+      Issue.all
     end
   end
 end
