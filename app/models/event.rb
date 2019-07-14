5fc615009c10199a099dfbfe1500b9bf5b84b324
Erick Takeshi <erick.tmr@outlook.com>
2019-07-14 14:41:47 -0300

    annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint           not null, primary key
+#  action     :string
+#  payload    :jsonb            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  issue_id   :bigint           not null
+#
+# Indexes
+#
+#  index_events_on_issue_id  (issue_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (issue_id => issues.id)
+#
+
 class Event < ApplicationRecord
   belongs_to :issue
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: issues
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  github_id  :bigint           not null
+#
+# Indexes
+#
+#  index_issues_on_github_id  (github_id) UNIQUE
+#
+
 class Issue < ApplicationRecord
   has_many :events, dependent: :destroy
 end
