4258654fafc83848fa73d1365c7f073d2257f3b7
Erick Takeshi <erick.tmr@outlook.com>
2019-07-14 20:17:28 -0300

    events index controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,26 @@
 module Api
   class EventsController < ApplicationController
     def index
-      
+      render_json_response(
+        resources: events,
+        serializer: EventSerializer
+      )
+    end
+
+    private
+
+    def events
+      return [] if requested_issue.nil?
+
+      @events ||= requested_issue.events
+    end
+
+    def issue_github_id
+      params[:issue_id]
+    end
+
+    def requested_issue
+      Issue.find_by(github_id: issue_github_id)
     end
   end
 end
