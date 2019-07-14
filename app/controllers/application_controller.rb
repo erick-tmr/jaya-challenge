1ce6ac0d76be89aea5853684012fe8b206aea323
Erick Takeshi <erick.tmr@outlook.com>
2019-07-14 20:16:58 -0300

    implement generic render json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+  ACTIVE_RECORDS_COLLECTION_CLASSES = [
+    'ActiveRecord::Relation',
+    'ActiveRecord::Associations::CollectionProxy'
+  ].freeze
+
+  protected
+
+  def render_json_response(opts = {})
+    resources = opts.fetch(:resources)
+    serializer = opts.fetch(:serializer)
+    options = opts.fetch(:options, {})
+    status = opts.fetch(:status, :ok)
+
+    serialized_response = serializer.new(
+      resources,
+      { is_collection: collection?(resources) }.merge(options)
+    ).serializable_hash
+
+    render json: serialized_response, status: status, content_type: 'application/vnd.api+json'
+  end
+
+  private
+
+  def collection?(resources)
+    resources.is_a?(Array) || ACTIVE_RECORDS_COLLECTION_CLASSES.include?(resources.class.name)
+  end
 end
