dfbc788bc8ef301837a2f5da4b6c497067a74718
Erick Takeshi <erick.tmr@outlook.com>
2019-07-14 14:42:02 -0300

    event processor

---
+++ app/services/event_processor.rb
@@ -0,0 +1,29 @@
+class EventProcessor
+  def initialize(opts = {})
+    @params = opts.fetch(:params, {})
+  end
+
+  def call
+    get_or_create_issue
+    create_event
+  end
+
+  private
+
+  def get_or_create_issue
+    @issue = Issue.find_by(github_id: @params[:issue_id])
+
+    return @issue if @issue.present?
+
+    @issue = Issue.create(
+      github_id: @params[:issue_id]
+    )
+  end
+
+  def create_event
+    new_event = Event.new(
+      action: @params[:action],
+      payload: @params[:payload]
+    )
+  end
+end
